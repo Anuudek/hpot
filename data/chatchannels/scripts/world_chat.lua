923fdf3dea6a29b36afbdae5be7420170b175d4f
Anuudek <140118333+Anuudek@users.noreply.github.com>
2024-05-24 18:46:37 -0300

    Update world_chat.lua

--- data/chatchannels/scripts/world_chat.lua
+++ data/chatchannels/scripts/world_chat.lua
@@ -1,8 +1,23 @@
+local CHANNEL_WORLD_CHAT = 3
+
+local muted = Condition(CONDITION_CHANNELMUTEDTICKS, CONDITIONID_DEFAULT)
+muted:setParameter(CONDITION_PARAM_SUBID, CHANNEL_WORLD_CHAT)
+muted:setParameter(CONDITION_PARAM_TICKS, 60000)
+
 function onSpeak(player, type, message)
 	local playerGroupType = player:getGroup():getId()
-	if player:getLevel() == 1 and playerGroupType < GROUP_TYPE_GAMEMASTER then
-		player:sendCancelMessage("You may not speak into channels as long as you are on level 1.")
+	if player:getLevel() < 100 and playerGroupType < GROUP_TYPE_GAMEMASTER then
+		player:sendCancelMessage("Only players level 100+ are allowed to send messages on this channel.")
 		return false
+	end
+
+	if player:getCondition(CONDITION_CHANNELMUTEDTICKS, CONDITIONID_DEFAULT, CHANNEL_WORLD_CHAT) then
+		player:sendCancelMessage("You may only send another message in 60 seconds.")
+		return false
+	end
+
+	if playerGroupType == GROUP_TYPE_NORMAL then
+		player:addCondition(muted)
 	end
 
 	if type == TALKTYPE_CHANNEL_Y then
